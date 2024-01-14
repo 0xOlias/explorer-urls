4eb82431211703357a22ac4b6eb5cfd83db89cbc
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-01-14 13:32:13 -0500

    feat: script

--- index.ts
+++ index.ts
@@ -1,15 +1,34 @@
 import * as chains from 'viem/chains'
+import { writeFileSync } from 'node:fs'
 
 const now = Math.floor(Date.now() / 1000)
 
-const latestBlockParams = new URLSearchParams({
+/**
+ * Each Etherscan deployment supports a different set of modules/actions.
+ * The blockgetblocknobytime action was supported by each deployment I
+ * checked manually, so it's a good candidate for testing.
+ * 
+ * Blockscout deployments also support a subset of Etherscan actions, so this
+ * test should work for them as well.
+ */
+const etherscanTestParams = new URLSearchParams({
   module: "block",
   action: "getblocknobytime",
   timestamp: String(now),
   closest: "before"
 });
 
-const results: { [chainName: string]: { [explorerName: string]: string } } = {}
+const results: {
+  [chainName: string]: { 
+    blockExplorers: { 
+      [explorerName: string]: {
+        name: string;
+        url: string;
+        apiUrl: string | undefined
+      } 
+    }
+  } 
+} = {}
 
 for (const [chainKey, chain] of Object.entries(chains)) {
 
@@ -18,12 +37,13 @@ for (const [chainKey, chain] of Object.entries(chains)) {
   for (const [explorerName, explorer] of explorers) {
     const url = new URL(explorer.url)
 
-    let apiBaseUrl = 'not found'
+    let apiBaseUrl: string | undefined = undefined
 
     const nameGuessOne = chain.name.replace(new RegExp(' ', 'g'), '')
     const nameGuessTwo = chainKey.replace(new RegExp(' ', 'g'), '')
 
     const prefixGuesses = [
+      "",
       "api-",
       "api.",
       `api-${nameGuessOne}.`,
@@ -44,13 +64,12 @@ for (const [chainKey, chain] of Object.entries(chains)) {
       console.log(`  attempt: ${i}`)
       
       try {
-        const guessUrl = new URL(`${url.protocol}//${prefix}${url.host}${suffix}?${latestBlockParams.toString()}`)
+        const guessUrl = new URL(`${url.protocol}//${prefix}${url.host}${suffix}?${etherscanTestParams.toString()}`)
         const guessBaseUrl = guessUrl.href.split("?")[0]
         console.log(`    guess: ${guessBaseUrl}`)
 
         const response = await fetch(guessUrl);
         const json = await response.json() as { status: string; result: string; message: string; }
-        console.log(response.status)
         if (response.status === 200 && json.status === "1") {
           console.log('    status: success')
           apiBaseUrl = guessBaseUrl
@@ -61,9 +80,12 @@ for (const [chainKey, chain] of Object.entries(chains)) {
       }
     }
 
-    (results[chain.name] ||= {})[explorerName] = apiBaseUrl
+    (results[chainKey] ||= { blockExplorers: {} }).blockExplorers[explorerName] = {
+      name: explorer.name,
+      url: explorer.url,
+      apiUrl: apiBaseUrl
+    }
   }
 }
 
-console.log(results)
-
+writeFileSync(`./results-${now}.json`, JSON.stringify(results, null, 2))
